ac56eba0068c824fffc0d1931b877e979db04b4a
Saugat Rasaili <saugatrasaili76@gmail.com>
2025-08-18 13:36:55 +0545

    added 2 new files

---
+++ day_1/userinput.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class userinput{
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Asking user for input
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+
+        // Printing the number
+        System.out.println(number);
+
+        scanner.close();
+    }
+}
