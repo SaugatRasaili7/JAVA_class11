014404b00cd2a291af4790e6c0198a08c00c7f6a
Saugat Rasaili <saugatrasaili76@gmail.com>
2025-11-03 22:05:57 +0545

    added new files

---
+++ day-2/Season_Month.java
@@ -0,0 +1,41 @@
+ import java.util.Scanner;
+
+public class Season_Month {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        
+        System.out.print("Enter month number (1-12): ");
+        int month = input.nextInt();
+       
+
+        switch (month) {
+
+             case 1: case 2: case 12:
+                System.out.println("Winter");
+                break;
+
+
+            case 3: case 4: case 5:
+                System.out.println("Spring");
+                break;
+
+
+            case 6: case 7: case 8:
+                System.out.println("Summer");
+                break;
+
+
+            case 9: case 10: case 11:
+              System.out.println("Autumn");
+                break;
+            
+            default:
+            System.out.println("Invalid Input!");
+        }
+
+        input.close();
+    }
+
+
+}
