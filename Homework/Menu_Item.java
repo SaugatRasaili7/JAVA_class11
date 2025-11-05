45a108bac9c60fd96dfe449319492e6bbdaab29d
Saugat Rasaili <saugatrasaili76@gmail.com>
2025-11-05 21:41:31 +0545

    updated

--- Homework/Menu_Item.java
+++ Homework/Menu_Item.java
@@ -4,15 +4,26 @@ public class Menu_Item {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        System.out.println("Enter your name :\n");
+        System.out.println("Menu Item :\n");
 
-        String name = input.nextLine();
+        System.out.println("1.Burger\n");
+
+        System.out.println("Pizza \n");
+
+        System.out.println("Onion Rings\n");
+        
+        System.out.println("Grilled Chicken Sandwich\n");
+
+         System.out.println("Garden Fresh Salad\n");
+
+        System.out.println("Coffee\n");
+
 
         System.out.println("Enter your Food choice : \n");
       
         String menuItem = input.nextLine();
 
-        System.out.println("The Entered Menu item by " + name +  " is " + menuItem);
+        System.out.println("The Entered Menu item is " + menuItem);
 
 
 
