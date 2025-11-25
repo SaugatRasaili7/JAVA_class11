162585d9c974a791c81429cb9d4b3a2159160cfa
Saugat Rasaili <saugatrasaili76@gmail.com>
2025-11-25 16:39:03 +0545

    updated

---
+++ day-2/Multiplication.java
@@ -0,0 +1,11 @@
+  import java.util.Scanner;
+  public class multiplication {
+
+    public static void main(String[] args){
+        
+        int n;
+
+        System.out.println("===Enter the number of which you wanted to display the Multiplication Table === \n");
+
+    }
+  }
