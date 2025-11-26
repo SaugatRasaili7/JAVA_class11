f8efdb651f15757c0ca5eb44021e4bb7df69baf0
Saugat Rasaili <saugatrasaili76@gmail.com>
2025-11-26 08:27:19 +0545

    updated Multiplication

--- day-2/Multiplication.java
+++ day-2/Multiplication.java
@@ -1,11 +1,17 @@
   import java.util.Scanner;
-  public class multiplication {
+  public class Multiplication {
 
     public static void main(String[] args){
-        
-        int n;
+      Scanner input = new Scanner(System.in);
+      
 
-        System.out.println("===Enter the number of which you wanted to display the Multiplication Table === \n");
+        System.out.println("===Enter the number of which you wanted to display the Multiplication Table up to 10 === \n");
+        int n = input.nextInt();
+        System.out.println("The Multiplication table of " + n + "is : \n");
+        
+        for(int i =1; i<=10; i++){
+          System.out.println(n + " * " + i +" = " + n*i);
+        }
 
     }
   }
